855daf6a81637edfa94e063b8dffc59591e2d270
sugarliu <450755513@qq.com>
2017-08-02 21:38:00 +0800

    finish test for no use promotions

--- src/best-charge.js
+++ src/best-charge.js
@@ -3,12 +3,14 @@ var loadAllItems = require('./items.js');
 var loadPromotions = require('./promotions.js');
 var allItems = loadAllItems();
 var promotionsInfo = loadPromotions();
+
 function bestCharge(selectedItem) {
   var inputInfo = inputService(selectedItem);
   var buyItemsInfo = buildItemsInfo(inputInfo,allItems);
   var bestCharge = calculatePromotions(buyItemsInfo,promotionsInfo);
- // return /*TODO*/;
+ // return /*TODO*/
 }
+
 function inputService(tags) {
   let inputInfo = [];
   for (let item of tags) {
@@ -25,30 +27,36 @@ function buildItemsInfo(inputInfo,allItems) {
   for (let obj of allItems) {
     for (let item of inputInfo) {
       if (item.id == obj.id){
-        let totalPrice = (item.count * obj.price).toFixed(2);
-        buyItemsInfo.push({ id: obj.id, count: item.count,name: obj.name, price: obj.price.toFixed(2), totalPrice: totalPrice })
+        let totalPrice = item.count * obj.price;
+        buyItemsInfo.push({ id: obj.id, count: item.count, name: obj.name, price: parseInt(obj.price), totalPrice: totalPrice })
       }
     }
   }
   return buyItemsInfo;
 }
-/**
+
 function calculatePromotions( buyItemsInfo, promotionsInfo ) {
   var bestCharge = [];
-
-  return bestCharge;
+  var typeOnePromotions = typeOneCharge ( buyItemsInfo, promotionsInfo[0].type);
+  var typeTwoPromotions = typeTwoCharge ( buyItemsInfo, promotionsInfo[1]);
+  console.log(typeOnePromotions);
+  if (typeOnePromotions[0].charge === 0 && typeTwoPromotions[0].charge === 0){
+    return 0;
+  }
+  
 }
-**/
+
 function typeOneCharge( buyItemsInfo, type) {
   let typeOnePromotions = [];
   let summary = 0;
-
+  let charge = 0;
   for (let obj of buyItemsInfo) {
     summary += parseInt(obj.totalPrice);
   }
   if (summary >= 30){
-    typeOnePromotions.push({type: type, charge: 6.00 })
+    charge = 6.00;
   }
+  typeOnePromotions.push({type: type, charge: charge });
   return typeOnePromotions;
 }
 
@@ -70,7 +78,7 @@ function typeTwoCharge(buyItemsInfo, promotionTwo) {
 }
 
 module.exports = { buildItemsInfo: buildItemsInfo, inputService: inputService, typeOneCharge: typeOneCharge,
-typeTwoCharge: typeTwoCharge};
+typeTwoCharge: typeTwoCharge, calculatePromotions: calculatePromotions};
 
 
 
