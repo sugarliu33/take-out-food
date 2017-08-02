b4186f42c7b6f005a68ede5a7060ee5c32656cd8
sugarliu <450755513@qq.com>
2017-08-02 14:10:48 +0800

    finish inputService test function

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,23 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
+'use strict';
+
+function bestCharge(selectedItem) {
+ // return /*TODO*/;
 }
+function inputService(tags) {
+  console.log(tags);
+  let inputInfo = [];
+  for (let item of tags) {
+    if(item.includes('x')){
+      let arrayItem = item.split('x');
+      inputInfo.push({id:arrayItem[0].trim(),count:parseInt(arrayItem[1].trim())});
+    }
+  }
+  console.log(inputInfo);
+  return inputInfo;
+}
+
+module.exports = inputService;
+
+
+
+
